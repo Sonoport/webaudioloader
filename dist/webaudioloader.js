7708fa23597b34ba567cb3dc015ab63f72f270f3
Chinmay Pendharkar <notthetup@gmail.com>
2014-12-12 16:44:07 +0800

    updated standlone file

--- dist/webaudioloader.js
+++ dist/webaudioloader.js
@@ -1,7 +1,11 @@
-/*webaudioloader - v1.0.0 - Fri Dec 12 2014 13:15:38 GMT+0800 (SGT) */ 
+/*webaudioloader - v1.0.0 - Fri Dec 12 2014 16:44:00 GMT+0800 (SGT) */ 
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.webaudioloader=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
+/*
+	constructor
+	var wal = new WebAudioLoader({cache : false, maxCacheSize : 1000, onload: function(){}, onprogress: function(){}, context : audioContext })
+ */
 function WebAudioLoader (options){
 
 	if ( !( this instanceof WebAudioLoader ) ) {
@@ -106,7 +110,12 @@ function WebAudioLoader (options){
 
 	};
 }
-
+/*
+	load method.
+	wal.load('http://www.example.com/audio.mp3');
+	wal.load([object File]);
+	wal.load('http://www.example.com/audio.mp3', {decode: false,cache : false , onload: function(){}, onprogress: function(){}});
+ */
 WebAudioLoader.prototype.load = function (source, options){
 
 	var decode =  true;
@@ -119,6 +128,8 @@ WebAudioLoader.prototype.load = function (source, options){
 
 	if (options.cache !== null && options.cache !== undefined){
 		thisLoadCache = options.cache;
+	}else{
+		thisLoadCache = this.cache;
 	}
 
 	if (options.decode !== null && options.decode !== undefined){
@@ -147,7 +158,7 @@ WebAudioLoader.prototype.load = function (source, options){
 			onLoadProxy(err,arrayBuffer);
 		}else{
 			this.context.decodeAudioData(arrayBuffer, function(audioBuffer){
-				if (thisLoadCache && this.cache){
+				if (thisLoadCache){
 					this._cachedAudio.set(source,audioBuffer);
 				}
 				onLoadProxy(err,audioBuffer);
@@ -158,8 +169,12 @@ WebAudioLoader.prototype.load = function (source, options){
 	}.bind(this));
 };
 
+/*
+	flushCache method
+	Resets and empties the cache.
+ */
 WebAudioLoader.prototype.flushCache = function (){
-	this._cache.reset();
+	this._cachedAudio.reset();
 };
 
 module.exports = WebAudioLoader;
