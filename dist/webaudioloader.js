7cd7f40f126faa4a7341835721cb2674298e743a
Chinmay Pendharkar <notthetup@gmail.com>
2015-03-10 13:12:50 +0800

    fix(package.json) fixed the main reference in package.json and other superficial changes

--- dist/webaudioloader.js
+++ dist/webaudioloader.js
@@ -1,4 +1,4 @@
-/*webaudioloader - v1.0.0 - Fri Dec 12 2014 16:44:00 GMT+0800 (SGT) */ 
+/*webaudioloader - v1.0.0 - Tue Mar 10 2015 13:08:21 GMT+0800 (SGT) */ 
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.webaudioloader=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
